8f93494c1036a7ca0a2902aefc5e2f8cf1557ced
Elaine <e.cristina00@gmail.com>
2023-02-19 17:53:17 -0300

    Aprendendo mais sobre Mockito

--- teste-java/src/test/java/com/mockito/MockitoTeste2.java
+++ teste-java/src/test/java/com/mockito/MockitoTeste2.java
@@ -2,24 +2,26 @@ package com.mockito;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MockitoTest {
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTeste2 {
+
+    @Mock
+    List<String> lista;
 
     @Test
     public void primeiroTesteMockito(){
-        List<String> lista = Mockito.mock(ArrayList.class);
         Mockito.when(lista.size() ).thenReturn(3);
-        lista.add("Teste1");
-        lista.add("teste2");
-        lista.add("teste3");
 
         int size = lista.size();
 
         Assertions.assertThat(size).isEqualTo(3);
     }
 }
-
